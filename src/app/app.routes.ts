159df899de765d2ace6a5c0cd99332afedd07aa4
KalyaJex <svslither@gmail.com>
2024-07-26 16:22:59 +0200

    wip login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'prefix',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
